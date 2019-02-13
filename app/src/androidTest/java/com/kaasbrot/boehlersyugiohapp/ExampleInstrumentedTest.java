6dd658dc7e88f69fef57e271965092d54b64a5fa
Programmierer <erifetim@gmail.com>
2019-02-13 22:27:36 +0100

    Adding history dialog

--- app/src/androidTest/java/com/kaasbrot/boehlersyugiohapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/kaasbrot/boehlersyugiohapp/ExampleInstrumentedTest.java
@@ -23,4 +23,54 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.kaasbrot.boehlersyugiohapp", appContext.getPackageName());
     }
+
+    /**
+     * Test basic action history actions
+     * - Setup for both players
+     * - Points changing
+     * - Undo / Redo
+     */
+    @Test
+    public void actionHistoryTest() {
+        ActionHistory history = new ActionHistory(8000, 8000);
+
+        // check initial setup
+        assertFalse(history.canUndo());
+        assertFalse(history.canRedo());
+        assertEquals(8000, history.getCurrentEntry().p1);
+        assertEquals(8000, history.getCurrentEntry().p2);
+
+        // undo shouldn't change anything
+        ActionHistory.Points p = history.undo();
+        assertEquals(8000, p.p1);
+        assertEquals(8000, p.p2);
+
+        // redo shouldn't change anything
+        p = history.redo();
+        assertEquals(8000, p.p1);
+        assertEquals(8000, p.p2);
+
+        // player 2 lost points
+        history.add(8000, 7000);
+        p = history.getCurrentEntry();
+
+        assertTrue(history.canUndo());
+        assertFalse(history.canRedo());
+        assertEquals(8000, history.getCurrentEntry().p1);
+        assertEquals(7000, history.getCurrentEntry().p2);
+
+        // undo last action
+        p = history.undo();
+        assertFalse(history.canUndo());
+        assertTrue(history.canRedo());
+        assertEquals(8000, history.getCurrentEntry().p1);
+        assertEquals(8000, history.getCurrentEntry().p2);
+
+        // redo last action
+        p = history.redo();
+        assertTrue(history.canUndo());
+        assertFalse(history.canRedo());
+        assertEquals(8000, history.getCurrentEntry().p1);
+        assertEquals(7000, history.getCurrentEntry().p2);
+    }
 }
