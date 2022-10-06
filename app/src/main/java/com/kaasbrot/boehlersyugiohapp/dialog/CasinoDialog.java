228994b248d60463b7bc6f440e4ebbb6d340d9e8
lawtrafalgar02 <46304349+lawtrafalgar02@users.noreply.github.com>
2022-10-07 00:22:47 +0200

    dice pics and small animation
    implemented dice pictures and made a first small animation

--- app/src/main/java/com/kaasbrot/boehlersyugiohapp/dialog/CasinoDialog.java
+++ app/src/main/java/com/kaasbrot/boehlersyugiohapp/dialog/CasinoDialog.java
@@ -2,11 +2,15 @@ package com.kaasbrot.boehlersyugiohapp.dialog;
 
 import android.app.Dialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatDialogFragment;
 import android.text.Html;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kaasbrot.boehlersyugiohapp.R;
@@ -14,6 +18,7 @@ import com.kaasbrot.boehlersyugiohapp.history.Coin;
 import com.kaasbrot.boehlersyugiohapp.history.Dice;
 import com.kaasbrot.boehlersyugiohapp.history.History;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 
 public class CasinoDialog extends AppCompatDialogFragment {
@@ -29,11 +34,56 @@ public class CasinoDialog extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext(), R.style.MyDialogTheme);
-        //c = new Coin(rand.nextBoolean() ? Coin.Toss.HEADS : Coin.Toss.TAILS);
         d = new Dice(rand.nextInt(6) + 1);
+        builder.setMessage(R.string.dice_title); //centered title with bigger font than default title
 
-        builder.setMessage(Html.fromHtml(d.asHtml(), Html.FROM_HTML_MODE_COMPACT))
-                .setTitle(R.string.dice_title);
+        LayoutInflater factory = LayoutInflater.from(getContext());
+        View view = factory.inflate(R.layout.dice_layout, null);
+
+        //define images, for rare dice source can be changed here with if statement
+        int[] imgPaths = {
+                R.drawable.d1,
+                R.drawable.d2,
+                R.drawable.d3,
+                R.drawable.d4,
+                R.drawable.d5,
+                R.drawable.d6
+        };
+
+        //defining variables that will be changed for animations
+        ImageView imaged;
+        imaged = view.findViewById(R.id.dicep);
+        imaged.setImageResource(imgPaths[1]);   //dice starts at 1
+        TextView textd1;                        //default size is 40, needs to get smaller
+        TextView textd2;                        //default size is 10, needs to get bigger
+        textd1 = view.findViewById(R.id.dicet1);
+        textd2 = view.findViewById(R.id.dicet2);
+        builder.setView(view);
+
+        //2 delays at the moment, will need for loop that does this x times
+        //textd1 and textd2 size should always add to the same number
+        //should read size out of dice_layout, add them and use that number
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                imaged.setImageResource(imgPaths[4]);
+                textd1.setTextSize(10);
+                textd2.setTextSize(40);
+                builder.setView(view);
+            }
+        },500);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                imaged.setImageResource(imgPaths[d.roll-1]); //final roll is correct
+                textd1.setTextSize(40);
+                textd2.setTextSize(10);
+                builder.setView(view);
+            }
+        },1000);
+
+        /*builder.setMessage(Html.fromHtml(d.asHtml(), Html.FROM_HTML_MODE_COMPACT))
+                .setTitle(R.string.dice_title);*/
         if(history != null) {
             //history.add(c);
             history.add(d);
@@ -47,7 +97,7 @@ public class CasinoDialog extends AppCompatDialogFragment {
         // once the dialog is built and shown, we can adjust text size and font alignment
         super.onStart();
         TextView texts = this.getDialog().findViewById(android.R.id.message);
-        texts.setTextSize(144);
+        texts.setTextSize(30);
         texts.setGravity(Gravity.CENTER);
     }
     public Dice getDice() {
